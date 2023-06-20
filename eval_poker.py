f5e387a65b8c25f5c53491cc3154011c33f9336b
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2023-06-20 17:07:26 -0400

    stuff
    s

---
+++ eval_poker.py
@@ -0,0 +1,219 @@
+import random
+from tqdm import tqdm
+from treys import Card
+
+
+def _to_treys_representation(card_list):
+    trey_cards = []
+    for x in card_list:
+        st = str(x[0]).upper() + x[1]
+        trey_cards.append(Card.new(st))
+    return trey_cards
+    
+
+    # board = [
+    # Card.new('Ah'),
+    # Card.new('Kd'),
+    # Card.new('Jc')
+
+
+def get_deck(exclude_me=None):
+    full_deck = []
+    for suit in ["s", "c", "h", "d"]:
+        for num in ["2", "3", "4", "5", "6", "7", "8", "9", "t", "j", "q", "k", "a"]:
+            if exclude_me:
+                if num + suit not in exclude_me:
+                    full_deck.append(num + suit)
+            else:
+                full_deck.append(num + suit)
+    random.shuffle(full_deck)
+    return full_deck
+
+
+
+def get_random_hands(n_other_players, remaining_cards, needed_flop_cards=0):
+    random.shuffle(remaining_cards)
+    all_cards_copy = remaining_cards.copy()
+    other_hands = []
+    for _ in range(n_other_players):
+        cards = [
+        all_cards_copy.pop(random.randint(0, len(all_cards_copy) - 1)) for _ in range(2)
+        ]
+        other_hands.append(cards)
+        
+    if needed_flop_cards > 0:
+        board_ext = [
+        all_cards_copy.pop(random.randint(0, len(all_cards_copy) - 1)) for _ in range(needed_flop_cards)
+        ]
+        return other_hands, board_ext
+    return other_hands
+
+def generate_game_start_state(my_board_representation, my_hand):
+    
+    exclude_me = my_hand.copy()
+    
+    if my_board_representation is not None:
+        exclude_me.extend(my_board_representation)
+    
+    remaining_cards = get_deck(exclude_me)
+    remaining_cards=sorted(remaining_cards)
+    
+    hand = _to_treys_representation(my_hand)
+    if my_board_representation:
+        board = _to_treys_representation(my_board_representation)
+    else:
+        board=None
+    remaining_cards = _to_treys_representation(remaining_cards)
+    
+    
+    result = 0
+    return remaining_cards, hand, board
+
+
+
+
+
+def get_winner(hand, other_hands, board, evaluator, fast=True):
+    player_score = evaluator.evaluate(board, hand)
+    player_rank = evaluator.get_rank_class(player_score)
+    # other_hands = [_to_treys_representation(x) for x in other_hands]
+    op_hand_scores = []
+    op_hand_ranks = []
+    if fast:
+        for x in other_hands:
+            score = evaluator.evaluate(board, x)
+            rank_class = evaluator.get_rank_class(score)
+            op_hand_ranks.append(rank_class)
+        if min(op_hand_ranks) < player_rank:
+            return -1
+        elif min(op_hand_ranks) == player_rank:
+            return 0
+        else: 
+            return 1
+    
+    for x in other_hands:
+        score = evaluator.evaluate(board, x)
+        rank_class = evaluator.get_rank_class(score)
+        op_hand_scores.append(score)
+        op_hand_ranks.append(rank_class)
+        
+    
+    for i in range(len(op_hand_scores)):
+        Card.print_pretty_cards(other_hands[i])
+        print(evaluator.class_to_string(op_hand_ranks[i]), op_hand_ranks[i])
+    
+    Card.print_pretty_cards(hand)
+    print(evaluator.class_to_string(player_rank))
+    print(player_rank)
+    print(player_score)
+    
+    
+    
+    
+
+def simulate_win_percent(my_board_representation, my_hand, num_sims, n_other_players=5, print_sim=False, print_ravg=False, decimal_places=None):
+    remaining_cards, hand, board = generate_game_start_state(my_board_representation, my_hand)
+
+
+    wins = 0
+    draws = 0
+    losses = 0
+    # if not board or len(board) < 5:
+    #     print(f"Need to generate  {5-len(board)} board cards each sim")
+    
+    win_rates = []
+
+    og_board = board 
+    pbar = tqdm(range(num_sims))
+    for i in pbar:
+        if og_board:
+            temp_board = og_board.copy()
+        else:
+            temp_board = None
+                
+        # For each sim
+        if temp_board:
+            if len(temp_board) == 5:
+                other_hands = get_random_hands(5, remaining_cards)
+            else:
+                other_hands, board_ext = get_random_hands(n_other_players, remaining_cards, needed_flop_cards=5-len(temp_board))
+                # print(board_ext)
+                # board_ext = _to_treys_representation(board_ext)
+                temp_board.extend(board_ext)
+        else:
+            other_hands, board_ext = get_random_hands(n_other_players, remaining_cards, needed_flop_cards=5)
+            temp_board = board_ext
+                
+            # board_ext = _to_treys_representation(board_ext)
+            # if board:
+                
+            # else:
+            #     board = board_ext
+            assert len(temp_board) == 5
+        from treys import Evaluator
+        evaluator = Evaluator()
+        if print_sim:
+            print("\n")
+            for x in other_hands:
+                Card.print_pretty_cards(x)
+            Card.print_pretty_cards(temp_board)
+        result = get_winner(hand, other_hands, temp_board, evaluator)
+
+        
+        if result == 1:
+            wins +=1 
+            if print_sim:
+                print("Win\n")
+        elif result == 0:
+            draws +=1
+            if print_sim:
+                print("Draw\n")
+            
+        elif result == -1:
+            losses +=1
+            if print_sim:
+                print("Loss\n")
+        
+        total_games = wins + draws + losses
+        avg = wins/total_games
+        win_rates.append(avg)
+        if print_ravg and i%10 == 0:
+            pbar.set_description(f"Running Average: {avg*100:{5}.{5}}%")
+        board=None
+    total_games = wins + draws + losses
+    avg = wins/total_games
+
+    if decimal_places > 0:
+        avg *= 100
+        avg = round(avg, decimal_places)
+<<<<<<< HEAD
+    return avg
+
+    return avg*100
+=======
+        return avg
+    return avg
+
+    # # return avg*100
+>>>>>>> ce9d61e3e0cf8280aa181b9e4ffb4b81c5c44fd8
+    
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+
+    # win_rates = [x * 100 for x in win_rates]
+    # x = np.arange(len(win_rates))
+
+    # plt.title("Win Rate Over Time")
+    # plt.plot(x, win_rates)
+    # plt.ylim([np.mean(win_rates) - 10, np.mean(win_rates) + 10])
+    # plt.xlabel("Number of Simulations")
+    # plt.ylabel("Win Rate %")
+    # plt.show()
+
+
+
+
+
+
+
+    
---
+++ main.py
@@ -0,0 +1,84 @@
+<<<<<<< HEAD
+from eval_poker import simulate_win_percent
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/implied_odds/")
+async def calculate_pot_odds(chance_percent: int = 1, current_pot: float = 1.0, amount_to_call: float = 1.0):
+    implied_odds_dollars = ((1 / (chance_percent/100.0)) * amount_to_call) - (current_pot + amount_to_call) 
+    return {"implied_odds_dollars": implied_odds_dollars}
+
+
+# @app.get("/g_bucks/")
+# async def calculate_pot_odds(chance_percent: int = 1, current_pot: float = 1.0, amount_to_call: float = 1.0):
+#     implied_odds_dollars = ((1 / (chance_percent/100.0)) * amount_to_call) - (current_pot + amount_to_call) 
+#     return {"implied_odds_dollars": implied_odds_dollars}
+
+
+=======
+from fastapi import FastAPI
+import uvicorn
+from mangum import Mangum
+app = FastAPI()
+handler = Mangum(app)
+from eval_poker import simulate_win_percent
+
+
+
+@app.get("/implied_odds/")
+async def calculate_implied_odds(chance_percent: int = 1, current_pot: float = 1.0, amount_to_call: float = 1.0):
+    implied_odds_dollars = ((1 / (chance_percent/100.0)) * amount_to_call) - (current_pot + amount_to_call) 
+    return {"implied_odds_dollars": implied_odds_dollars}
+
+>>>>>>> ce9d61e3e0cf8280aa181b9e4ffb4b81c5c44fd8
+@app.get("/pot_odds/")
+async def calculate_pot_odds(current_pot: float = 1.0, bet: float = 1.0):
+    pot_odds = (current_pot + bet ) /bet
+    return {"pot_odds": pot_odds}
+
+
+
+<<<<<<< HEAD
+
+num_sims = 1000
+
+
+# my_board_representation = ["3c", "3d", "7s"]
+# my_hand = ["3h", "4s"] 
+
+
+
+
+
+@app.get("/get_win_rate/")
+async def calculate_pot_odds(my_board_representation: str = "",  my_hand:str = "", num_sims: int = 1000):
+
+    my_board_representation = [str(x) for x in my_board_representation.split(",")]
+    my_hand = [str(x) for x in my_hand.split(",")]
+    print(my_hand)
+    win_percent = simulate_win_percent(my_board_representation, my_hand, num_sims, n_other_players=3,print_sim=False, print_ravg=True, decimal_places=2)
+    return {"win_percent": win_percent}
+=======
+@app.get("/get_win_rate/")
+async def get_win_rate(my_board_representation: str = "", my_hand: str = "", num_sims: int = 100, n_other_players: int = 5):
+    if my_board_representation == "":
+        # return {"x":my_board_representation}
+        my_board_representation = None
+    else:
+        my_board_representation = my_board_representation.split(',')
+    my_hand = my_hand.split(',')
+        
+    
+    win_rate = simulate_win_percent(my_board_representation, my_hand, num_sims, n_other_players=5,print_sim=False, print_ravg=True, decimal_places=2)
+    
+    return {"win_rate": win_rate}
+
+
+>>>>>>> ce9d61e3e0cf8280aa181b9e4ffb4b81c5c44fd8
