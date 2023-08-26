42781aebb5dc19cc69ff92edcdc9c5a8abfbf519
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2023-08-25 20:26:51 -0400

    Updated shit

--- eval_poker.py
+++ eval_poker.py
@@ -1,244 +1,205 @@
 import random
 from tqdm import tqdm
-# from treys import Card
+from card import Card
+import itertools
+from typing import Sequence, List
+from lookup import LookupTable
 
-from typing import Sequence
 
-class Card:
+class Evaluator:
     """
-    Static class that handles cards. We represent cards as 32-bit integers, so 
-    there is no object instantiation - they are just ints. Most of the bits are 
-    used, and have a specific meaning. See below: 
+    Evaluates hand strengths using a variant of Cactus Kev's algorithm:
+    http://suffe.cool/poker/evaluator.html
 
-                                    Card:
-
-                          bitrank     suit rank   prime
-                    +--------+--------+--------+--------+
-                    |xxxbbbbb|bbbbbbbb|cdhsrrrr|xxpppppp|
-                    +--------+--------+--------+--------+
-
-        1) p = prime number of rank (deuce=2,trey=3,four=5,...,ace=41)
-        2) r = rank of card (deuce=0,trey=1,four=2,five=3,...,ace=12)
-        3) cdhs = suit of card (bit turned on based on suit of card)
-        4) b = bit turned on depending on rank of card
-        5) x = unused
-
-    This representation will allow us to do very important things like:
-    - Make a unique prime prodcut for each hand
-    - Detect flushes
-    - Detect straights
-
-    and is also quite performant.
+    I make considerable optimizations in terms of speed and memory usage, 
+    in fact the lookup table generation can be done in under a second and 
+    consequent evaluations are very fast. Won't beat C, but very fast as 
+    all calculations are done with bit arithmetic and table lookups. 
     """
 
-    # the basics
-    STR_RANKS: str = '23456789TJQKA'
-    STR_SUITS: str = 'shdc'
-    INT_RANKS: range = range(13)
-    
-    from typing import List
-# tricks: List[str] =`` []
-    PRIMES: List[int] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    HAND_LENGTH = 2
+    BOARD_LENGTH = 5
 
-    # conversion from string => int
-    CHAR_RANK_TO_INT_RANK: dict[str, int] = dict(zip(list(STR_RANKS), INT_RANKS))
-    CHAR_SUIT_TO_INT_SUIT: dict[str, int] = {
-        's': 1,  # spades
-        'h': 2,  # hearts
-        'd': 4,  # diamonds
-        'c': 8,  # clubs
-        '\u2660': 1, # spades (unicode)
-        '\u2764': 2, # hearts (unicode)
-        '\u2666': 4, # diamonds (unicode)
-        '\u2663': 8, # clubs (unicode)
-    }
-    INT_SUIT_TO_CHAR_SUIT: str = 'xshxdxxxc'
+    def __init__(self) -> None:
 
-    # for pretty printing
-    PRETTY_SUITS: dict[int, str] = {
-        1: chr(9824),   # spades
-        2: chr(9829),   # hearts
-        4: chr(9830),   # diamonds
-        8: chr(9827)    # clubs
-    }
-
-    SUIT_COLORS: dict[int, str] = {
-        2: "red",
-        4: "blue",
-        8: "green"
-    }
-
-    @staticmethod
-    def new(string: str) -> int:
-        """
-        Converts Card string to binary integer representation of card, inspired by:
+        self.table = LookupTable()
         
-        http://www.suffecool.net/poker/evaluator.html
+        self.hand_size_map = {
+            5: self._five,
+            6: self._six,
+            7: self._seven
+        }
+
+    def evaluate(self, hand: List[int], board: List[int]) -> int:
+        """
+        This is the function that the user calls to get a hand rank. 
+
+        No input validation because that's cycles!
+        """
+        all_cards = hand + board
+        return self.hand_size_map[len(all_cards)](all_cards)
+
+    def _five(self, cards: Sequence[int]) -> int:
+        """
+        Performs an evalution given cards in integer form, mapping them to
+        a rank in the range [1, 7462], with lower ranks being more powerful.
+
+        Variant of Cactus Kev's 5 card evaluator, though I saved a lot of memory
+        space using a hash table and condensing some of the calculations. 
+        """
+        # if flush
+        if cards[0] & cards[1] & cards[2] & cards[3] & cards[4] & 0xF000:
+            handOR = (cards[0] | cards[1] | cards[2] | cards[3] | cards[4]) >> 16
+            prime = Card.prime_product_from_rankbits(handOR)
+            return self.table.flush_lookup[prime]
+
+        # otherwise
+        else:
+            prime = Card.prime_product_from_hand(cards)
+            return self.table.unsuited_lookup[prime]
+
+    def _six(self, cards: Sequence[int]) -> int:
+        """
+        Performs five_card_eval() on all (6 choose 5) = 6 subsets
+        of 5 cards in the set of 6 to determine the best ranking, 
+        and returns this ranking.
+        """
+        minimum = LookupTable.MAX_HIGH_CARD
+
+        for combo in itertools.combinations(cards, 5):
+
+            score = self._five(combo)
+            if score < minimum:
+                minimum = score
+
+        return minimum
+
+    def _seven(self, cards: Sequence[int]) -> int:
+        """
+        Performs five_card_eval() on all (7 choose 5) = 21 subsets
+        of 5 cards in the set of 7 to determine the best ranking, 
+        and returns this ranking.
+        """
+        minimum = LookupTable.MAX_HIGH_CARD
+
+        for combo in itertools.combinations(cards, 5):
+            
+            score = self._five(combo)
+            if score < minimum:
+                minimum = score
+
+        return minimum
+
+    def get_rank_class(self, hr: int) -> int:
+        """
+        Returns the class of hand given the hand hand_rank
+        returned from evaluate. 
+        """
+        if hr >= 0 and hr <= LookupTable.MAX_ROYAL_FLUSH:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_ROYAL_FLUSH]
+        elif hr <= LookupTable.MAX_STRAIGHT_FLUSH:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT_FLUSH]
+        elif hr <= LookupTable.MAX_FOUR_OF_A_KIND:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FOUR_OF_A_KIND]
+        elif hr <= LookupTable.MAX_FULL_HOUSE:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FULL_HOUSE]
+        elif hr <= LookupTable.MAX_FLUSH:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FLUSH]
+        elif hr <= LookupTable.MAX_STRAIGHT:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT]
+        elif hr <= LookupTable.MAX_THREE_OF_A_KIND:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_THREE_OF_A_KIND]
+        elif hr <= LookupTable.MAX_TWO_PAIR:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_TWO_PAIR]
+        elif hr <= LookupTable.MAX_PAIR:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_PAIR]
+        elif hr <= LookupTable.MAX_HIGH_CARD:
+            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_HIGH_CARD]
+        else:
+            raise Exception("Inavlid hand rank, cannot return rank class")
+
+    def class_to_string(self, class_int: int) -> str:
+        """
+        Converts the integer class hand score into a human-readable string.
+        """
+        return LookupTable.RANK_CLASS_TO_STRING[class_int]
+
+    def get_five_card_rank_percentage(self, hand_rank: int) -> float:
+        """
+        Scales the hand rank score to the [0.0, 1.0] range.
+        """
+        return float(hand_rank) / float(LookupTable.MAX_HIGH_CARD)
+
+    def hand_summary(self, board: List[int], hands: List[List[int]]) -> None:
+        """
+        Gives a sumamry of the hand with ranks as time proceeds. 
+
+        Requires that the board is in chronological order for the 
+        analysis to make sense.
         """
 
-        rank_char = string[0]
-        suit_char = string[1]
-        rank_int = Card.CHAR_RANK_TO_INT_RANK[rank_char]
-        suit_int = Card.CHAR_SUIT_TO_INT_SUIT[suit_char]
-        rank_prime = Card.PRIMES[rank_int]
+        assert len(board) == self.BOARD_LENGTH, "Invalid board length"
+        for hand in hands:
+            assert len(hand) == self.HAND_LENGTH, "Invalid hand length"
 
-        bitrank = 1 << rank_int << 16
-        suit = suit_int << 12
-        rank = rank_int << 8
+        line_length = 10
+        stages = ["FLOP", "TURN", "RIVER"]
 
-        return bitrank | suit | rank | rank_prime
+        for i in range(len(stages)):
+            line = "=" * line_length
+            print("{} {} {}".format(line,stages[i],line))
+            
+            best_rank = 7463  # rank one worse than worst hand
+            winners = []
+            for player, hand in enumerate(hands):
 
-    @staticmethod
-    def int_to_str(card_int: int) -> str:
-        rank_int = Card.get_rank_int(card_int)
-        suit_int = Card.get_suit_int(card_int)
-        return Card.STR_RANKS[rank_int] + Card.INT_SUIT_TO_CHAR_SUIT[suit_int]
+                # evaluate current board position
+                rank = self.evaluate(hand, board[:(i + 3)])
+                rank_class = self.get_rank_class(rank)
+                class_string = self.class_to_string(rank_class)
+                percentage = 1.0 - self.get_five_card_rank_percentage(rank)  # higher better here
+                print("Player {} hand = {}, percentage rank among all hands = {}".format(player + 1, class_string, percentage))
 
-    @staticmethod
-    def get_rank_int(card_int: int) -> int:
-        return (card_int >> 8) & 0xF
+                # detect winner
+                if rank == best_rank:
+                    winners.append(player)
+                    best_rank = rank
+                elif rank < best_rank:
+                    winners = [player]
+                    best_rank = rank
 
-    @staticmethod
-    def get_suit_int(card_int: int) -> int:
-        return (card_int >> 12) & 0xF
+            # if we're not on the river
+            if i != stages.index("RIVER"):
+                if len(winners) == 1:
+                    print("Player {} hand is currently winning.\n".format(winners[0] + 1))
+                else:
+                    print("Players {} are tied for the lead.\n".format([x + 1 for x in winners]))
 
-    @staticmethod
-    def get_bitrank_int(card_int: int) -> int:
-        return (card_int >> 16) & 0x1FFF
-
-    @staticmethod
-    def get_prime(card_int: int) -> int:
-        return card_int & 0x3F
-
-    @staticmethod
-    def hand_to_binary(card_strs: Sequence[str]) -> list[int]:
-        """
-        Expects a list of cards as strings and returns a list
-        of integers of same length corresponding to those strings. 
-        """
-        bhand = []
-        for c in card_strs:
-            bhand.append(Card.new(c))
-        return bhand
-
-    @staticmethod
-    def prime_product_from_hand(card_ints: Sequence[int]) -> int:
-        """
-        Expects a list of cards in integer form. 
-        """
-
-        product = 1
-        for c in card_ints:
-            product *= (c & 0xFF)
-
-        return product
-
-    @staticmethod
-    def prime_product_from_rankbits(rankbits: int) -> int:
-        """
-        Returns the prime product using the bitrank (b)
-        bits of the hand. Each 1 in the sequence is converted
-        to the correct prime and multiplied in.
-
-        Params:
-            rankbits = a single 32-bit (only 13-bits set) integer representing 
-                    the ranks of 5 _different_ ranked cards 
-                    (5 of 13 bits are set)
-
-        Primarily used for evaulating flushes and straights, 
-        two occasions where we know the ranks are *ALL* different.
-
-        Assumes that the input is in form (set bits):
-
-                              rankbits     
-                        +--------+--------+
-                        |xxxbbbbb|bbbbbbbb|
-                        +--------+--------+
-
-        """
-        product = 1
-        for i in Card.INT_RANKS:
-            # if the ith bit is set
-            if rankbits & (1 << i):
-                product *= Card.PRIMES[i]
-
-        return product
-
-    @staticmethod
-    def int_to_binary(card_int: int) -> str:
-        """
-        For debugging purposes. Displays the binary number as a 
-        human readable string in groups of four digits. 
-        """
-        bstr = bin(card_int)[2:][::-1]  # chop off the 0b and THEN reverse string
-        output = list("".join(["0000" + "\t"] * 7) + "0000")
-
-        for i in range(len(bstr)):
-            output[i + int(i/4)] = bstr[i]
-
-        # output the string to console
-        output.reverse()
-        return "".join(output)
-
-    @staticmethod
-    def int_to_pretty_str(card_int: int) -> str:
-        """
-        Prints a single card 
-        """
-        
-        color = False
-        try:
-            from termcolor import colored
-            # for mac, linux: http://pypi.python.org/pypi/termcolor
-            # can use for windows: http://pypi.python.org/pypi/colorama
-            color = True
-        except ImportError: 
-            pass
-
-        # suit and rank
-        suit_int = Card.get_suit_int(card_int)
-        rank_int = Card.get_rank_int(card_int)
-
-        # color
-        s = Card.PRETTY_SUITS[suit_int]
-        if color and suit_int in Card.SUIT_COLORS:
-            s = colored(s, Card.SUIT_COLORS[suit_int])
-
-        r = Card.STR_RANKS[rank_int]
-
-        return "[{}{}]".format(r,s)
-
-    @staticmethod
-    def print_pretty_card(card_int: int) -> None:
-        """
-        Expects a single integer as input
-        """
-        print(Card.int_to_pretty_str(card_int))
-
-    @staticmethod
-    def ints_to_pretty_str(card_ints: Sequence[int]) -> str:
-        """
-        Expects a list of cards in integer form.
-        """
-        output = " "
-        for i in range(len(card_ints)):
-            c = card_ints[i]
-            if i != len(card_ints) - 1:
-                output += str(Card.int_to_pretty_str(c)) + ","
+            # otherwise on all other streets
             else:
-                output += str(Card.int_to_pretty_str(c)) + " "
-    
-        return output
+                hand_result = self.class_to_string(self.get_rank_class(self.evaluate(hands[winners[0]], board)))
+                print()
+                print("{} HAND OVER {}".format(line, line))
+                if len(winners) == 1:
+                    print("Player {} is the winner with a {}\n".format(winners[0] + 1, hand_result))
+                else:
+                    print("Players {} tied for the win with a {}\n".format([x + 1 for x in winners],hand_result))
 
-    @staticmethod
-    def print_pretty_cards(card_ints: Sequence[int]) -> None:
-        """
-        Expects a list of cards in integer form.
-        """
-        print(Card.ints_to_pretty_str(card_ints))
-        
-        
-        
+
+class PLOEvaluator(Evaluator):
+
+    HAND_LENGTH = 4
+
+    def evaluate(self, hand: List[int], board: List[int]) -> int:
+        minimum = LookupTable.MAX_HIGH_CARD
+
+        for hand_combo in itertools.combinations(hand, 2):
+            for board_combo in itertools.combinations(board, 3):
+                score = Evaluator._five(self, list(board_combo) + list(hand_combo))
+                if score < minimum:
+                    minimum = score
+
+        return minimum
 
 
 def _to_treys_representation(card_list):
@@ -388,7 +349,7 @@ def simulate_win_percent(my_board_representation, my_hand, num_sims, n_other_pla
             # else:
             #     board = board_ext
             assert len(temp_board) == 5
-        from treys import Evaluator
+        # from evaluator import Evaluator
         evaluator = Evaluator()
         if print_sim:
             print("\n")
