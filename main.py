3dfa732e42bc9a7acda30f8b5510da73584ffa29
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2023-09-22 20:23:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ async def calculate_pot_odds(my_board_representation: str = "",  my_hand:str = "
 @app.get("/get_win_rate/")
 async def get_win_rate(my_board_representation: str = "", my_hand: str = "", num_sims: int = 100, n_other_players: int = 5):
     print(f"my_board_representation: {my_board_representation}")
-    if my_board_representation == "" or my_board_representation == '':
+    if my_board_representation == "" or my_board_representation == '' or my_board_representation == ['']:
         # return {"x":my_board_representation}
         my_board_representation = None
     else:
